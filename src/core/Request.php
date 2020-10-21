d3456a495707a6345c43970e46db5c3a601efe31
Fumiaki Hisaoka <sipuofare631@gmail.com>
2020-10-22 00:40:13 +0900

    add:url parse method

--- src/core/Request.php
+++ src/core/Request.php
@@ -85,4 +85,47 @@ class Request
     {
         return $_SERVER['REQUEST_URL'];
     }
+
+    /**
+     * Retrieve base url
+     *
+     * @return string
+     */
+    public function getBaseUrl(): string
+    {
+        $request_uri = $this->getRequestUri();
+        $script_name = $_SERVER['SCRIPT_NAME'];
+
+        // included script file name in url
+        if (0 === strpos($request_uri, $script_name)) {
+            return $script_name;
+        }
+        // prefix match script file directory with url
+        $script_directory = dirname($script_name);
+        if (0 === strpos($request_uri, $script_directory)) {
+            return rtrim($script_directory, '/');
+        }
+
+        return '';
+    }
+
+    /**
+     * Retrieve url path(behind base url)
+     *
+     * @return string
+     */
+    public function getPathInfo(): string
+    {
+        $request_uri = $this->getRequestUri();
+        $base_url = $this->getBaseUrl();
+
+        $query_position = strpos($request_uri, '?');
+        if (false !== $query_position) {
+            $request_uri = substr($request_uri, 0, $query_position);
+        }
+
+        $path_info = substr($request_uri, strlen($base_url));
+
+        return (string)$path_info;
+    }
 }
