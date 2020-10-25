b51b31900bf7d313cb13da055c95628d16404d4c
Fumiaki Hisaoka <sipuofare631@gmail.com>
2020-10-25 22:51:31 +0900

    add:Controller class. Constructor method

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -2,7 +2,36 @@
 
 abstract class Controller
 {
-    // TODO
+    /** @var string */
+    protected $controller_name;
+    /** @var string */
+    protected $action_name;
+    /** @var Application */
+    protected $application;
+    /** @var Request */
+    protected $request;
+    /** @var Response */
+    protected $response;
+    /** @var Session */
+    protected $session;
+    /** @var DbManager */
+    protected $db_manager;
+
+    /**
+     * Constructor method
+     *
+     * @param Application $application
+     */
+    public function __construct(Application $application)
+    {
+        $this->controller_name = strtolower(substr(get_class($this), 0, -10));
+
+        $this->application = $application;
+        $this->request = $application->getRequest();
+        $this->response = $application->getResponse();
+        $this->session = $application->getSession();
+        $this->db_manager = $application->getDbManager();
+    }
 
     /**
      * Retrieve the response content
