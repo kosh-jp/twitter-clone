0db2818c5725e1737166a2f804750ade56952543
Fumiaki Hisaoka <sipuofare631@gmail.com>
2020-10-25 09:05:04 +0900

    add:DbRepository class. Table handler

--- src/core/DbRepository.php
+++ src/core/DbRepository.php
@@ -2,5 +2,69 @@
 
 abstract class DbRepository
 {
-    // TODO
+    /** @var PDO */
+    protected $con;
+
+    /**
+     * Constructor method
+     * Set PDO instance
+     *
+     * @param PDO $con
+     */
+    public function __construct(PDO $con)
+    {
+        $this->setConnection($con);
+    }
+
+    /**
+     * Set PDO instance
+     *
+     * @param PDO $con
+     * @return void
+     */
+    public function setConnection(PDO $con): void
+    {
+        $this->con = $con;
+    }
+
+    /**
+     * Retrieve an executed prepared statement
+     *
+     * @param string $sql
+     * @param array<string,string> $params
+     * @return PDOStatement
+     */
+    public function execute(string $sql, array $params = []): PDOStatement
+    {
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute($params);
+
+        return $stmt;
+    }
+
+    /**
+     * Run fetch method against the database
+     *
+     * @param string $sql
+     * @param array<string,string> $params
+     * @return array<string,string>
+     */
+    public function fetch(string $sql, array $params = []): array
+    {
+        return $this->execute($sql, $params)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Run fetchAll method against the database
+     *
+     * @param string $sql
+     * @param array<string,string> $params
+     * @return array<string,string>
+     */
+    public function fetchAll(string $sql, array $params = []): array
+    {
+        $result = $this->execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result ?: [];
+    }
 }
