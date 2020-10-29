79cf7b7e9e78648748fc7e1d9a0348e47222e3fe
Fumiaki Hisaoka <sipuofare631@gmail.com>
2020-10-30 01:25:19 +0900

    add:UserRepository. Model of User. fix:UserTable. Password column.

--- src/models/create_init_20201029000000.sql
+++ src/models/create_init_20201029000000.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user(
   id INTEGER AUTO_INCREMENT,
   user_name VARCHAR(20) NOT NULL,
-  password VARCHAR(40) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   created_at DATETIME,
   PRIMARY KEY(id),
   UNIQUE KEY user_name_index(user_name)
