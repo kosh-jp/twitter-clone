383f0ff5c150326938867a3c866b04ccf0ba7f68
Fumiaki Hisaoka <sipuofare631@gmail.com>
2020-10-31 23:53:47 +0900

    add:registerAction. modify:signupTemplate. Display error message

--- src/views/account/signup.php
+++ src/views/account/signup.php
@@ -1,3 +1,7 @@
+<?php
+
+/** @var View $this */
+?>
 <?php $this->setLayoutVar('title', 'アカウント登録') ?>
 
 <h2>アカウント登録</h2>
@@ -5,18 +9,26 @@
 <form action="<?= $base_url ?>/account/register" method="post">
     <input type="hidden" name="_token" value="<?= $this->escape($_token) ?>">
 
+    <?php if (!empty($errors)) : ?>
+        <ul class="error_list">
+            <?php foreach ($errors as $error) : ?>
+                <li><?= $this->escape($error) ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
     <table>
         <tbody>
             <tr>
                 <th>ユーザーID</th>
                 <td>
-                    <input type="text" name="user_name" value="">
+                    <input type="text" name="user_name" value="<?= $this->escape($user_name) ?>">
                 </td>
             </tr>
             <tr>
                 <th>パスワード</th>
                 <td>
-                    <input type="password" name="password" value="">
+                    <input type="password" name="password" value="<?= $this->escape($password) ?>">
                 </td>
             </tr>
         </tbody>
