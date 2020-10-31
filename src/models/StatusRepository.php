e926433833bab28eb47ec6ce72f367896b27fd74
Fumiaki Hisaoka <sipuofare631@gmail.com>
2020-11-01 08:35:10 +0900

    add:StatusRepository. Model of status. fix:UserRepository. Type hinting

---
+++ src/models/StatusRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+class StatusRepository extends DbRepository
+{
+    /**
+     * @param string $user_id
+     * @param string $body
+     * @return void
+     */
+    public function insert(string $user_id, string $body): void
+    {
+        $now = new DateTime();
+        $created_at = $now->format('Y-m-d H:i:s');
+
+        $sql = "INSERT INTO status(user_id, body, created_at)
+            VALUE (:user_id, :body, :created_at)";
+
+        $stmt = $this->execute($sql, compact('user_id', 'body', 'created_at'));
+    }
+
+    /**
+     * @param string $user_id
+     * @return array<string,string>
+     */
+    public function fetchAllPersonalArchivesByUserId(string $user_id): array
+    {
+        $sql = "SELECT status.*, user.user_name
+                FROM status
+                    LEFT JOIN user ON status.user_id = user.id
+                WHERE user.id = :user_id
+                ORDER BY status.created_at DESC";
+
+        return $this->fetchAll($sql, compact('user_id'));
+    }
+}
